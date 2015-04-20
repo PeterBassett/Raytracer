e1604c970971c1e505843d7304fc913d14892269
PeterBassett <peter.bassett@gmail.com>
2015-04-20 09:01:59 +0100

    Futher Performance Improvements in Image Loading
    Multithreading added to the image loader fast path.

--- Rendering/FileTypes/ImageReader.cs
+++ Rendering/FileTypes/ImageReader.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using Raytracer.Rendering.Core;
-using Size = Raytracer.MathTypes.Size;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Raytracer.Rendering.FileTypes
 {
     static class ImageReader
     {
-        public static Bmp Read(System.Drawing.Bitmap other)
+        public static Bmp Read(Bitmap other)
         {            
             var bmp = new Bmp(other.Width, other.Height);
 
@@ -17,7 +17,7 @@ namespace Raytracer.Rendering.FileTypes
             return bmp;
         }
 
-        private static void ReadImage(System.Drawing.Bitmap other, Bmp bmp)
+        private static void ReadImage(Bitmap other, Bmp bmp)
         {
             var reader = GetPixelReader(other);
 
@@ -27,26 +27,30 @@ namespace Raytracer.Rendering.FileTypes
                 ReadSlowFallback(bmp, other);
         }
 
-        private static void ReadImageFast(Bmp bmp, System.Drawing.Bitmap other)
+        private static void ReadImageFast(Bmp bmp, Bitmap other)
         {
             var depth = Image.GetPixelFormatSize(other.PixelFormat);
             var components = depth / 8;
 
             var data = other.LockBits(new Rectangle(0, 0, other.Width, other.Height), ImageLockMode.ReadWrite, other.PixelFormat);
-            
-            unsafe
+
+            try
             {
                 var getPixel = GetPixelReader(other);
-                
-                for (int y = 0; y < data.Height; y++)
-                    for (int x = 0; x < data.Width; x++)
+
+                Parallel.For(0, data.Height, y =>
+                {
+                    for (var x = 0; x < data.Width; x++)
                         bmp.SetPixel(x, y, getPixel(data, components, x, y));
+                });
             }
-           
-            other.UnlockBits(data);
+            finally
+            {
+                other.UnlockBits(data);
+            }
         }
 
-        private static void ReadSlowFallback(Bmp bmp, System.Drawing.Bitmap other)
+        private static void ReadSlowFallback(Bmp bmp, Bitmap other)
         {
             for (int x = 0; x < bmp.Size.Width; x++)
             {
@@ -62,22 +66,30 @@ namespace Raytracer.Rendering.FileTypes
             }
         }
 
-        private static unsafe Func<BitmapData, int, int, int, Colour> GetPixelReader(Bitmap data)
-        {
+        private static Func<BitmapData, int, int, int, Colour> GetPixelReader(Bitmap data)
+        {            
             switch (data.PixelFormat)
             {
                 case PixelFormat.Format32bppArgb:
                 case PixelFormat.Format32bppPArgb:
                 case PixelFormat.Format24bppRgb:
-                    return GetPixel32and24;
+                    return GetPixel32And24;
                 case PixelFormat.Format8bppIndexed:
-                    return (BitmapData bmpData, int components, int x, int y) => GetPixel8BppIndexed(bmpData, components, x, y, data);
+                {
+                    //store the palette here so we can use it in multiple threads via the lambda below.
+                    var palette = data.Palette.Entries.ToArray();
+
+                    // ReSharper disable once RedundantLambdaParameterType
+                    return
+                        (BitmapData bmpData, int components, int x, int y) =>
+                            GetPixel8BppIndexed(bmpData, components, x, y, palette);
+                }
                 default:
-                    throw new ArgumentOutOfRangeException("depth is not supported");
+                    throw new ArgumentOutOfRangeException("data");
             }
         }
 
-        private static unsafe Colour GetPixel32and24(BitmapData bmpData, int components, int x, int y)
+        private static unsafe Colour GetPixel32And24(BitmapData bmpData, int components, int x, int y)
         {
             byte* row = (byte*)bmpData.Scan0 + (y * bmpData.Stride);
             int index = (x * components);
@@ -87,11 +99,11 @@ namespace Raytracer.Rendering.FileTypes
                               row[index] / 255.0);
         }
 
-        private static unsafe Colour GetPixel8BppIndexed(BitmapData bmpData, int components, int x, int y, Bitmap data)
+        private static unsafe Colour GetPixel8BppIndexed(BitmapData bmpData, int components, int x, int y, Color [] palette)
         {
             byte* row = (byte*)bmpData.Scan0 + (y * bmpData.Stride);
             int index = row[x];
-            var col = data.Palette.Entries[index];
+            var col = palette[index];
             return new Colour(col.R / 255.0, col.G / 255.0, col.B / 255.0);        
         }
     }
