b6b936788e19e657a5c32b8cf6123cb3a3b02136
Pete Bassett <peter.bassett@gmail.com>
2015-05-20 22:14:02 +0100

    More materials

--- Rendering/FileTypes/XMLRayScene/Loaders/Primitives/SphereParser.cs
+++ Rendering/FileTypes/XMLRayScene/Loaders/Primitives/SphereParser.cs
@@ -24,10 +24,15 @@ namespace Raytracer.Rendering.FileTypes.XMLRayScene.Loaders.Primitives
 	        if(mat == null)
 		        throw new Exception("Cannot find material '" + materialName + "' for sphere.");
 
+            var rotate = transform.GetObjectSpaceRotation();
+            var rotation = new Vector(MathLib.Rad2Deg(rotate.X),
+                                      MathLib.Rad2Deg(rotate.Y),
+                                      MathLib.Rad2Deg(rotate.Z));
+
             return new Sphere()
             {
                 Pos = transform.ToObjectSpace(new Point(0, 0, 0)),
-                Ori = transform.ToObjectSpace(new Vector(0, 1, 0)),
+                Ori = rotation,
                 Radius = radius,
                 Material = mat
             };
