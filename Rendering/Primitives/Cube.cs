162b537a0b8c14f6d32d5e37ce3d3c6d78e0b329
Pete Bassett <peter.bassett@gmail.com>
2015-05-29 17:09:44 +0100

    Found refracion issues

--- Rendering/Primitives/Cube.cs
+++ Rendering/Primitives/Cube.cs
@@ -6,115 +6,123 @@ namespace Raytracer.Rendering.Primitives
 {
     class Cube : ObjectSpacePrimitive
     {
-        public Cube(Transform transform) : base(transform)
+        private const double HalfWidth = 0.5; // plane distance for unit cube
+        private const int AXIS_X = 0;
+        private const int AXIS_Y = 1;
+        private const int AXIS_Z = 2;
+
+        public Cube(Transform transform)
+            : base(transform)
         {
         }
 
         protected override IntersectionInfo ObjectSpaceIntersect(Ray ray)
         {
-	        var S = ray.Pos;
-	        var c = ray.Dir;
+            var position = ray.Pos;
+            var direction = ray.Dir;
 
-	        const double D = 0.5; // plane distance for unit cube
+            double t1, t2;
+            double tNear;
+            double tFar;
+            int tNear_index = 0;
+            int tFar_index = 0;
 
-	        double t1, t2;
-	        double tnear, tfar;
-	        int tnear_index=0, tfar_index=0;
+            if (ParallelWithPlane(ray, AXIS_X))
+                return new IntersectionInfo(HitResult.Miss);
 
-	        // intersect with the X planes
-	        if (c.X == 0 && (S.X < -D || S.X > D))
-		        return new IntersectionInfo(HitResult.Miss);
-	        else
-	        {
-		        tnear = (-D - S.X) / c.X;
-		        tfar = (D - S.X) / c.X;
+            CalculateNearAndFarIntersectionForAxis(ray, AXIS_X, out tNear, out tFar);
 
-		        if (tnear > tfar)
-			        Swap(ref tnear, ref tfar);
+            if (tNear > tFar)
+                Swap(ref tNear, ref tFar);
 
-		        if (tnear > tfar)
-			        return new IntersectionInfo(HitResult.Miss);
-		        if (tfar < 0)
-			        return new IntersectionInfo(HitResult.Miss);
-	        }
+            if (InvalidIntersection(tNear, tFar))
+                return new IntersectionInfo(HitResult.Miss);
 
-	        // intersect with the Y planes
-	        if (c.Y == 0 && (S.Y < -D || S.Y > D))
-		        return new IntersectionInfo(HitResult.Miss);
-	        else
-	        {
-		        t1 = (-D - S.Y) / c.Y;
-		        t2 = (D - S.Y) / c.Y;
+            // intersect with the Y planes
+            if (ParallelWithPlane(ray, AXIS_Y))
+                return new IntersectionInfo(HitResult.Miss);
 
-		        if (t1 > t2)
-                    Swap(ref t1, ref t2);
+            CalculateNearAndFarIntersectionForAxis(ray, AXIS_Y, out t1, out t2);
 
-		        if (t1 > tnear)
-		        {
-			        tnear = t1;
-			        tnear_index = 1;
-		        }
+            PickSmallestHitDistances(AXIS_Y, ref t1, ref t2, ref tNear, ref tFar, ref tNear_index, ref tFar_index);
 
-		        if (t2 < tfar)
-		        {
-			        tfar = t2;
-			        tfar_index = 1;
-		        }
+            if (InvalidIntersection(tNear, tFar))
+                return new IntersectionInfo(HitResult.Miss);
 
-		        if (tnear > tfar)
-			        return new IntersectionInfo(HitResult.Miss);
-		        if (tfar < 0)
-			        return new IntersectionInfo(HitResult.Miss);
-	        }
+            // intersect with the Z planes
+            if (ParallelWithPlane(ray, AXIS_Z))
+                return new IntersectionInfo(HitResult.Miss);
 
-	        // intersect with the Z planes
-	        if (c.Z == 0 && (S.Z < -D || S.Z > D))
-		        return new IntersectionInfo(HitResult.Miss);
-	        else
-	        {
-		        t1 = (-D - S.Z) / c.Z;
-		        t2 = (D - S.Z) / c.Z;
+            CalculateNearAndFarIntersectionForAxis(ray, AXIS_Z, out t1, out t2);
 
-		        if (t1 > t2)
-                    Swap(ref t1, ref t2);
+            PickSmallestHitDistances(AXIS_Z, ref t1, ref t2, ref tNear, ref tFar, ref tNear_index, ref tFar_index);
 
-		        if (t1 > tnear)
-		        {
-			        tnear = t1;
-			        tnear_index = 2;
-		        }
+            if (InvalidIntersection(tNear, tFar))
+                return new IntersectionInfo(HitResult.Miss);
 
-		        if (t2 < tfar)
-		        {
-			        tfar = t2;
-			        tfar_index = 2;
-		        }
-
-		        if (tnear > tfar)
-			        return new IntersectionInfo(HitResult.Miss);
-		        if (tfar < 0)
-			        return new IntersectionInfo(HitResult.Miss);
-	        }
-
-	        var normals = new [] {new Normal(1,0,0), new Normal(0,1,0), new Normal(0,0,1)};
+            var normals = new[] { new Normal(1, 0, 0), new Normal(0, 1, 0), new Normal(0, 0, 1) };
 
             IntersectionInfo intersection;
 
-	        if (tnear <= 0)
-	        {
-                intersection = new IntersectionInfo(HitResult.Hit, this, tfar, ray.Pos + ray.Dir * tfar, ray.Pos + ray.Dir * tfar, normals[tfar_index]);
+            if (tNear <= 0)
+            {
+                if (tFar == 0)
+                    return new IntersectionInfo(HitResult.Miss);
+
+                var hitPoint = ray.Pos + ray.Dir * tFar;
+                intersection = new IntersectionInfo(HitResult.Hit, this, tFar, hitPoint, hitPoint, normals[tFar_index]);
             }
-	        else
-	        {
-                intersection = new IntersectionInfo(HitResult.Hit, this, tnear, ray.Pos + ray.Dir * tnear, ray.Pos + ray.Dir * tnear, normals[tnear_index]);
+            else
+            {
+                var hitPoint = ray.Pos + ray.Dir * tNear;
+                intersection = new IntersectionInfo(HitResult.Hit, this, tNear, hitPoint, hitPoint, normals[tNear_index]);
             }
 
-	        if (Vector.DotProduct(intersection.NormalAtHitPoint, c) > 0)
-		        intersection.NormalAtHitPoint = -intersection.NormalAtHitPoint;
+            if (Vector.DotProduct(intersection.NormalAtHitPoint, direction) > 0)
+                intersection.NormalAtHitPoint = -intersection.NormalAtHitPoint;
 
-	        intersection.NormalAtHitPoint = intersection.NormalAtHitPoint.Normalize();
+            intersection.NormalAtHitPoint = intersection.NormalAtHitPoint.Normalize();
 
-	        return intersection; 
+            return intersection;
+        }
+
+        private bool InvalidIntersection(double tNear, double tFar)
+        {
+            if (tNear > tFar)
+                return true;
+            if (tFar < 0)
+                return true;
+
+            return false;
+        }
+
+        private void PickSmallestHitDistances(int axis, ref double t1, ref double t2, ref double tNear, ref double tfar, ref int tNear_index, ref int tFar_index)
+        {
+            if (t1 > t2)
+                Swap(ref t1, ref t2);
+
+            if (t1 > tNear)
+            {
+                tNear = t1;
+                tNear_index = axis;
+            }
+
+            if (t2 < tfar)
+            {
+                tfar = t2;
+                tFar_index = axis;
+            }
+        }
+
+        private bool ParallelWithPlane(Ray ray, int axis)
+        {
+            return ray.Dir[axis] == 0 && (ray.Pos[axis] < -HalfWidth || ray.Pos[axis] > HalfWidth);
+        }
+
+        private void CalculateNearAndFarIntersectionForAxis(Ray ray, int axis, out double tNear, out double tfar)
+        {
+            tNear = (-HalfWidth - ray.Pos[axis]) / ray.Dir[axis];
+            tfar = (HalfWidth - ray.Pos[axis]) / ray.Dir[axis];
         }
 
         protected override bool ObjectSpaceContains(Point point)
